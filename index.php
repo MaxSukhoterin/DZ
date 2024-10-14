86b7ba249b4fe7d5053db02924d6706ba3401ee5
MaxSukhoterin <161084415+MaxSukhoterin@users.noreply.github.com>
2024-10-14 15:45:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$text = "[   34  555   8 9 9  ]";
+$probil1 = "   ";
+$probil2 = "  ";
+$probil3 = " ";
+
+$replace = str_replace($probil1, $probil3, $text);
+
+$replace1 = str_replace($probil2, $probil3, $replace);
+
+print_r($replace1);
